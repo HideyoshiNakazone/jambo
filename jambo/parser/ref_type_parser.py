94e04bb4bb73c92ae811fd7797e8be9f6a9c81ee
Vitor Hideyoshi <vitor.h.n.batista@gmail.com>
2025-04-21 01:01:35 -0300

    Initial Test Implementation of RefTypeParser

---
+++ jambo/parser/ref_type_parser.py
@@ -0,0 +1,66 @@
+from urllib import parse
+from jambo.parser._type_parser import GenericTypeParser
+
+from typing import ForwardRef
+
+
+class RefTypeParser(GenericTypeParser):
+    mapped_type = any
+
+    json_schema_type = "$ref"
+
+    type_mappings = { ... }
+
+    def from_properties(self, properties, name, required=False):
+        if "$ref" not in properties:
+            raise ValueError(
+                f"RefTypeParser: Missing $ref in properties for {name}"
+            )
+        
+        if "$context" not in properties:
+            raise RuntimeError(
+                f"RefTypeParser: Missing $content in properties for {name}"
+            )
+        
+        if not properties["$ref"].startswith("#"):
+            raise ValueError(
+                "At the moment, only local references are supported. Look into $defs and # for recursive references."
+            )
+        
+        ref_type = None
+        mapped_properties = dict()
+
+        if properties["$ref"] == "#":
+            if "title" not in properties["$context"]:
+                raise ValueError(
+                    "RefTypeParser: Missing title in properties for $ref #"
+                )
+            
+            ref_type = ForwardRef(properties["$context"]["title"])
+
+        elif properties["$ref"].startswith("#/$defs/"):
+            target_property = properties["$context"]
+            for prop_name in properties["$ref"].split("/")[1:]:
+                if prop_name not in target_property:
+                    raise ValueError(
+                        f"RefTypeParser: Missing {prop_name} in properties for $ref {properties['$ref']}"
+                    )
+                target_property = target_property[prop_name]
+
+            if "type" not in target_property:
+
+
+            parsed_type, parsed_properties = GenericTypeParser.get_impl(
+
+            ).from_properties()
+
+        else:
+            raise ValueError(
+                "RefTypeParser: Invalid $ref format. Only local references are supported."
+            )
+        
+        if not required:
+            mapped_properties["default"] = None
+
+            
+        return ref_type, mapped_properties
